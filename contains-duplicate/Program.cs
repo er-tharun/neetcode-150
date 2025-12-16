1a0ac79b69d7302b12bbd2a358b0afa5def43b81
Tharun Ravi <tharunravi98@gmail.com>
2025-12-16 08:48:57 +0530

    Initial commit

---
+++ contains-duplicate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace contains_duplicate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/contains-duplicate/
+        Console.WriteLine(ContainsDuplicate(new int[] {1,2,3,1}));
+    }
+
+    public static bool ContainsDuplicate(int[] nums) 
+    {
+        return new HashSet<int>(nums).Count < nums.Length;
+    }
+}
