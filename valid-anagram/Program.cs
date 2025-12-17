cbb645cbf1bf021e0f513c4eed3ad05836ff6e70
Tharun Ravi <tharunravi98@gmail.com>
2025-12-17 08:38:08 +0530

    valid anagram

---
+++ valid-anagram/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace valid_anagram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/valid-anagram/
+        Console.WriteLine(IsAnagram("anagram","nagaram"));
+    }
+
+    public static bool IsAnagram(string s, string t) {
+
+        if(s.Length != t.Length)
+            return false;
+
+        IDictionary<char, int> src = BuildDictonary(s.ToArray());
+        IDictionary<char, int> target = BuildDictonary(t.ToArray());
+
+        foreach(var itr in src)
+        {
+            if(!target.ContainsKey(itr.Key) || src[itr.Key] != target[itr.Key])
+                return false;
+        }
+        return true;
+
+    }
+
+    private static IDictionary<char, int> BuildDictonary(char[] data)
+    {
+        IDictionary<char, int> result = new Dictionary<char, int>();
+        Array.Sort(data);
+
+        foreach(var itr in data)
+        {
+            if(!result.ContainsKey(itr))
+                result.Add(itr, 1);
+            else
+                result[itr]++;
+        }
+        return result;
+    }
+}
