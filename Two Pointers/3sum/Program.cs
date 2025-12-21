da322f091fb7b1daed8c9e6707c74ef1f372c9d0
Tharun Ravi <tharunravi98@gmail.com>
2025-12-21 11:19:18 +0530

    3 sum

---
+++ Two Pointers/3sum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace _3sum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/3sum/
+        var result = ThreeSum(new int[] {-1,0,1,2,-1,-4});
+        StringBuilder sb = new StringBuilder();
+        sb.Append("[");
+        foreach(var itr in result)
+        {
+            sb.Append("[");
+            foreach(var lst in itr)
+            {
+                sb.Append(lst + " ");
+            }
+            sb.Append("]");
+        }
+        sb.Append("]");
+        Console.WriteLine(sb.ToString());
+    }
+
+    public static IList<IList<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        int n = nums.Length;
+        IList<IList<int>> result = new List<IList<int>>();
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] > 0)
+                break;
+            if(i> 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int l = i+1;
+            int r = n - 1;
+            while(l < r)
+            {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0)
+                    r--;
+                else if(sum < 0)
+                    l++;
+                else
+                {
+                    result.Add(new List<int>{nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while(l<r && nums[l] == nums[l-1])
+                        l++;
+                }
+            }
+        }
+        return result;
+    }
+
+}
