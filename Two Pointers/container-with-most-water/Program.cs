17a26bb7e759dd7bdd4931d4fb3332e3ede914fb
Tharun Ravi <tharunravi98@gmail.com>
2025-12-22 08:20:00 +0530

    Container with most water

---
+++ Two Pointers/container-with-most-water/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace container_with_most_water;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/container-with-most-water/
+        Console.WriteLine(MaxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+    }
+
+    public static int MaxArea(int[] height) 
+    {
+        int l = 0;
+        int r = height.Length-1;
+        int result = 0;
+
+        while(l < r)
+        {
+            result = Math.Max(result, (Math.Min(height[l], height[r]) * (r - l)));
+            if(height[l] <= height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return result;
+    }
+}
