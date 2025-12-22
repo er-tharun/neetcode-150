e104b0f97e502f7d551b16dd9ae5108ae530ab3d
Tharun Ravi <tharunravi98@gmail.com>
2025-12-22 08:00:00 +0530

    code push for palindrome

---
+++ Two Pointers/valid-palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace valid_palindrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static bool IsPalindrome(string s) {
+        int l = 0;
+        int r = s.Length - 1;
+
+        var arr = s.ToCharArray();
+        while(l<r)
+        {
+            while(l<r &&!IsAlphaNumeric(arr[l]))
+                l++;
+            while(l < r &&!IsAlphaNumeric(arr[r]))
+                r--;
+            if(arr[l].ToString().ToLower() == arr[r].ToString().ToLower())
+            {
+                l++;
+                r--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    private static bool IsAlphaNumeric(char ch) => Char.IsLetterOrDigit(ch);
+}
