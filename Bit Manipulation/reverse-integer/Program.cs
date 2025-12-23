de8d37e8ddd1ede92aa0be1313a85191e814e8cc
Tharun Ravi <tharunravi98@gmail.com>
2025-12-23 20:41:00 +0530

    code push

---
+++ Bit Manipulation/reverse-integer/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace reverse_integer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/reverse-integer/
+        Console.WriteLine(Reverse(98652));
+    }
+
+    public static int Reverse(int x) {
+        int MIN = int.MinValue;
+        int MAX = int.MaxValue;
+        int res = 0;
+
+        while(x != 0)
+        {
+            int digit =  x%10;
+            x /= 10;
+
+            if(res > MAX/10 || (res == MAX/10 && digit > MAX/10))
+                return 0;
+            if(res < MIN/10 || (res == MIN/10 && digit < MIN/10))
+                return 0;
+
+            res = (res * 10) + digit;
+        }
+
+        return res;
+    }
+}
