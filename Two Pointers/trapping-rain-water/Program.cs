c1cda175de8d917194d622d087a78601e15b73c8
Tharun Ravi <tharunravi98@gmail.com>
2025-12-23 08:53:51 +0530

    Code push

---
+++ Two Pointers/trapping-rain-water/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace trapping_rain_water;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://leetcode.com/problems/trapping-rain-water/
+        Console.WriteLine(Trap(new int[] {0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+
+    public static int Trap(int[] height) {
+        int l = 0;
+        int r = height.Length - 1;
+
+        int result = 0;
+
+        int leftMax = height[l];
+        int rightMax = height[r];
+
+        while(l<r)
+        {
+            if(leftMax < rightMax)
+            {
+                l++;
+                leftMax = Math.Max(leftMax, height[l]);
+                result += leftMax - height[l];
+            }
+            else
+            {
+                r--;
+                rightMax = Math.Max(rightMax, height[r]);
+                result += rightMax - height[r];
+            }
+        }
+        return result;
+    }
+}
